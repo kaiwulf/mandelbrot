9b016bd090df19c3ab99b461caf53f584b69329b
kaism <eigenspaces@gmail.com>
2021-02-23 21:56:59 -0500

    iteration

--- dbrot/mandelbrot.d
+++ dbrot/mandelbrot.d
@@ -1,13 +1,30 @@
-use std.complex
+import std.complex, std.stdio;
 
-auto mandelbrot(real c_re, real c_im) {
+uint mandelbrot(real c_re, real c_im) {
     auto z = complex(0, 0);
+    immutable c = complex(c_re, c_im);
     uint n = 0;
     immutable MAX_ITER = 80;
-    while(abs(z) <= 2 && n < MAX_ITER ) {
-        z = z*z + c;
-        n += 1;
-    }
+    // while(abs(z) <= 2 && n < MAX_ITER ) {
+    //     z = z*z + c;
+    //     n += 1;
+    // }
 
-    return z;
+    foreach (_; 0 .. 100)
+        z = z ^^ 2 + complex(x, y);
+    write(z.abs < 2 ? '#' : '.');
+
+    return n;
+}
+
+void main() {
+   
+//    write(complex(1,1), " ", mandelbrot(1,1));
+
+    for(real i = -10.0; i <= 10.0; i += 1) {
+        for(real j = -10.0; j <= 10.0; j += 1) {
+            auto c = complex(i / 10, j / 10);
+            write(c, " ", mandelbrot(c.re, c.im), "\n");
+        }
+    }
 }
--- dbrot/plot.d
+++ dbrot/plot.d
@@ -1,19 +1,21 @@
-use.std
+void main() {
+    // Image size (pixels)
+    immutable WIDTH = 600;
+    immutable HEIGHT = 400;
 
-// Image size (pixels)
-immutable WIDTH = 600
-immutable HEIGHT = 400
+    // Plot window
+    immutable RE_START = -2;
+    immutable RE_END = 1;
+    immutable IM_START = -1;
+    immutable IM_END = 1;
 
-// Plot window
-immutable RE_START = -2
-immutable RE_END = 1
-immutable IM_START = -1
-immutable IM_END = 1
-
-foreach(x; 0 .. WIDTH) {
-    foreach(y; 0 .. HEIGHT) {
-        auto c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
-                    IM_START + (y / HEIGHT) * (IM_END - IM_START))
-        auto m = mandelbrot(c.re, c.im)
+    for(real x = 0.0; x < WIDTH; x += 0.1) {
+        for(real y = 0.0; x < HEIGHT; y += 0.1) {
+            auto c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
+                        IM_START + (y / HEIGHT) * (IM_END - IM_START));
+            auto m = mandelbrot(c.re, c.im);
+            
+        }
+        writeln
     }
 }
